ea69b9679458ac4695f0506c4ee2c4374014d0b9
caramujosan <gustavocjorge11@yahoo.com.br>
2026-09-05 01:07:03 -0300

    Fix OLLAMA_URL

--- Bootcamp Bradesco - GenAI, Dados & Cyber/dio-lab-bia-do-futuro-Projeto_Final/src/app.py
+++ Bootcamp Bradesco - GenAI, Dados & Cyber/dio-lab-bia-do-futuro-Projeto_Final/src/app.py
@@ -5,7 +5,7 @@ import streamlit as st
 
 
 # ============ CONFIGURAÇÃO ============
-OLLAMA_URL = "http://localhost:11434"
+OLLAMA_URL = "http://localhost:11434/api/generate"
 MODELO = "gpt-oss:20b"
 
 # ============ CARREGAR DADOS ============
